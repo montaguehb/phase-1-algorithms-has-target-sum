c0901d29c4be3c096cd2de0ff6268106cf12d2aa
Historia Montague <historia.montague@outlook.com>
2023-04-20 17:15:45 -0600

    Added initial algorithm, not very optimized.

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
+//First attempt without sorting/removing impossible elements 
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const start = performance.now()
+  for(let n = 0; n < array.length; n++) {
+    const num = array[n]
+    if(!!array.find((number) => number !== num?number === (target-num):false)) {
+      return [true, (performance.now() - start)]
+    }
+  }
+  return [false, (performance.now() - start)]
 }
+
+
+const createRandomTest = (arrayLength) => {
+  const target = Math.floor(Math.random() * 100)
+  const newArray = []
+  for(let n = 0; n < arrayLength; n++) {
+    newArray.push(Math.floor(Math.random() * 100))
+  }
+
+  console.log(hasTargetSum(newArray, target), newArray, target)
+} 
 
 /* 
   Write the Big O time complexity of your function here
+  O(n!) If you add each element to every other element of the array, the time complexity increases by nC2 where repeat combos are removed which can be represented by n!/2! > N!/2 removing all constants, we are left with a time complexity of n!
+  It's possible to improve this slightly by removing any numbers that are over the desired value as no matter, assuming no negative numbers, they will never equal a lower number with simple addition
+
+  what if I subtract a value from the target and do a search to see if that value exists within the array? will implement this part first and compare to later versions using sorted arrays. will also make a function to generate arrays of n size to test for very large arrays.
+  Is it faster if I also sort the array and check starting from the [n+1] value? or would a sort take longer than just searching through the array for every number?
+  If I sort before removing number larger than the target I can remove all values for (n, array.length). Would that be faster as well?
+  what would the time complexity be in either case?
+
+  JS does some weird stuff with sort e.g. when sorting the array [3, 8, 12, 4, 11, 7] using node, the result is [11, 12, 3, 4, 7, 8] instead of being ascending/descending probably has to do with how it converts each number into a string and compares the UTF-16 values for each number.
+
+  found out a bit more about sorting algorithms and introsort seems to be a good algorithm for quickly sorting an array, in place, using as little space as possible. Using https://www.techiedelight.com/introsort-algorithm/ as reference for how I might build it in JS
+
+  How can I optimize for space? current space complexity is O(n)
 */
 
 /* 
   Add your pseudocode here
+  sorts the array using the built in method
+  removes any element larger than the target
+  
 */
 
 /*
@@ -29,6 +65,8 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+  
+  createRandomTest(10000000)
 }
 
 module.exports = hasTargetSum;
